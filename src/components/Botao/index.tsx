11f45966e4eff83eedd208700f066df973a0f30e
Nsimba Tamaku <efrahimtks@gmail.com>
2022-10-29 14:03:51 +0100

    Adicionando a regressiva

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import style from "./Botao.module.scss";
 
 export default class Botao extends React.Component<{
-  type?: "button" | "submit" | undefined;
-  children?: React.ReactNode;
+  type?: "button" | "submit" | undefined,
+  children?: React.ReactNode,
+  onClick?: () => void
 }> {
   render() {
-    const { type = "button" } = this.props;
+    const { type = "button", onClick } = this.props;
     return (
-      <button type={type} className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.children}
       </button>
     );
--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,31 +1,42 @@
-import Botao from "../Botao"
-import Relogio from "./Relogio"
-import style from "./Cronometro.module.scss"
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from "./Cronometro.module.scss";
 import { tempoParaSegundos } from "../../common/utils/time";
 import { ITarefa } from "../../types/tarefa";
 import { useEffect, useState } from "react";
 interface Props {
-    selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined;
 }
 
-export default function Cronometro ({selecionado}: Props) {
-    console.log("conversao: ", tempoParaSegundos("01:30:01"));
-    const [tempo, setTempo] = useState<number>()
-    useEffect(() => {
-        if (selecionado?.tempo) {
-            setTempo(tempoParaSegundos(selecionado.tempo))
+export default function Cronometro({ selecionado }: Props) {
+  console.log("conversao: ", tempoParaSegundos("01:30:01"));
+  const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+        if (contador > 0) {
+            setTempo(contador - 1)
+            return regressiva(contador - 1)            
         }
-    }, [selecionado])
-    return (
-        <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
-            Tempo: {tempo}
-            <div className={style.relogioWrapper}>
-                <Relogio />
-            </div>
-            <Botao>
-                Comecar
-            </Botao>
-        </div>
-    )
+    }, 1000)
+  }
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronometro</p>
+      <div className={style.relogioWrapper}>
+        <Relogio tempo={tempo} />
+      </div>
+      <Botao
+        onClick={() => {
+          regressiva(tempo);
+        }}
+      >
+        Comecar
+      </Botao>
+    </div>
+  );
 }
